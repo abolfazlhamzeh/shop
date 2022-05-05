415af9fbe0224443945311ee79176e53bc0ef050
abolfazlhamzeh <a.zandi2008@gmail.com>
2022-05-05 17:59:52 +0430

    add customer class

--- Shop/Shop/Program.cs
+++ Shop/Shop/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using Shop;
+var obj = new Castomer();
+
+Console.WriteLine("enter your Id:");
+obj.CustomerId=int.Parse(Console.ReadLine());
+
+
+Console.WriteLine("enter your Name:");
+obj.CustomerName =Console.ReadLine();
+
+Console.WriteLine("enter your Address:");
+obj. Address = Console.ReadLine();
+
+
+Console.WriteLine("Id");
+Console.WriteLine("Name");
+Console.WriteLine("Adress");
+
+
+
+
+
+
+
+
+
+
+
